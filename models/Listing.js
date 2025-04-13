8f7e188fc878539c73ae9692f9a9115b27b66b03
Carson Loveless <78831392+Carson-Loveless@users.noreply.github.com>
2025-04-12 17:15:44 -0700

    Created Listing and User models.

---
+++ models/Listing.js
@@ -0,0 +1,35 @@
+const sequelize  = require('../db');
+const { Model, DataTypes } = require('sequelize');
+
+class Listing extends Model {}
+Listing.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    quality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    userid: {
+        type: DataTypes.INTEGER,
+    },
+}, {
+    sequelize,
+    modelName: 'Listing',
+});
+
+module.exports = Listing;
